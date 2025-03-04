3ea89f0810e4d04d0cca0e447e6f9d4f7c7a507e
Midhun Murali <midhunmurali776@gmail.com>
2025-03-04 16:31:28 +0530

    Bottom Navigation and each features just framed up

---
+++ lib/features/live_class/screens/live_class_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LiveClassScreen extends StatelessWidget {
+  const LiveClassScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Live Class Screen'),
+      ),
+    );
+  }
+}
