9fd794f391003fd68f661d1a8937a0365e62c2f6
Midhun Murali <midhunmurali776@gmail.com>
2025-03-06 17:49:26 +0530

    Course details page corrected

--- lib/features/coursedetails/screens/course_overview.dart
+++ lib/features/coursedetails/screens/course_overview.dart
@@ -19,7 +19,7 @@ class _CourseOverviewState extends State<CourseOverview> {
             decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(15)),
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -28,7 +28,7 @@ class _CourseOverviewState extends State<CourseOverview> {
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
                           SizedBox(height: 20),
                   Text(
-                      "The Foundation of 10 is a fundamental concept in mathematics, especially in number systems and arithmetic. It refers to the base-10 (decimal) number system, which is the most commonly used system for counting and calculations worldwide.Understanding the Foundation of 10 helps build strong mathematical skills, making it easier to learn advanced topics in mathematics and science.Overall, It provides  clear and efficient way to represent numbers, understand their size and value, and perform the operations that allow us to solve problems in everyday life.",
+                      "The Foundation of 10 is a fundamental concept in mathematics, especially in number systems and arithmetic. It refers to the base-10 (decimal) number system, which is the most commonly used system for counting and calculations worldwide.Understanding the Foundation of 10 helps build strong mathematical skills, making it easier to learn advanced topics in mathematics and science.Overall, It provides  clear and efficient way to represent numbers, understand their size and value, and perform the operations that allow us to solve problems in everyday life. The Foundation of 10 is a fundamental concept in mathematics, especially in number systems and arithmetic. It refers to the base-10 (decimal) number system, which is the most commonly used system for counting and calculations worldwide.Understanding the Foundation of 10 helps build strong mathematical skills, making it easier to learn advanced topics in mathematics and science.Overall, It provides  clear and efficient way to represent numbers, understand their size and value, and perform the operations that allow us to solve problems in everyday life.",
                      style: TextStyle(fontSize: 18)),
                      SizedBox(height: 20),
                 ],
