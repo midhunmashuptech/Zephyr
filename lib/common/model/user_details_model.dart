a1b57e4e5da89911196225339cf74f54c3a3b6b7
Midhun Murali <midhunmurali776@gmail.com>
2025-09-03 09:59:29 +0530

    Splash Screen Logic Completed

--- lib/common/model/user_details_model.dart
+++ lib/common/model/user_details_model.dart
@@ -3,8 +3,20 @@ import 'package:zephyr/common/model/user.dart';
 class UserDetailsModel {
   String? type;
   User? user;
+
+  UserDetailsModel({this.type, this.user});
+
   UserDetailsModel.fromJson(Map<String, dynamic> json) {
     type = json['type'];
-    user = json['user'];
+    user = json['user'] != null ? new User.fromJson(json['user']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['type'] = this.type;
+    if (this.user != null) {
+      data['user'] = this.user!.toJson();
+    }
+    return data;
   }
 }
