0459ac35fa093da9e4eb742ab2b5ba01ddcd5b8c
Midhun Murali <midhunmurali776@gmail.com>
2025-09-03 10:39:07 +0530

    Course Detail Screen Webview Done

--- lib/features/coursedetails/screens/course_overview.dart
+++ lib/features/coursedetails/screens/course_overview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:provider/provider.dart';
 import 'package:zephyr/features/coursedetails/provider/course_provider.dart';
 
@@ -12,8 +13,6 @@ class CourseOverview extends StatefulWidget {
 class _CourseOverviewState extends State<CourseOverview> {
   CourseProvider courseProvider = CourseProvider();
 
-
-
   @override
   Widget build(BuildContext context) {
     courseProvider = context.watch<CourseProvider>();
@@ -25,17 +24,20 @@ class _CourseOverviewState extends State<CourseOverview> {
             decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(15)),
             child: Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("About the course",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w600, fontSize: 22)),
-                  SizedBox(height: 20),
-                  Text(courseProvider.courseData.description ?? "Course description"
-                      ,style: TextStyle(fontSize: 18)),
-                  SizedBox(height: 20),
+                  // Center(
+                  //   child: Text("About the course",
+                  //       style:
+                  //           TextStyle(fontWeight: FontWeight.w600, fontSize: 22)),
+                  // ),
+                  // SizedBox(height: 10),
+                  Html(
+                    data: courseProvider.courseData.description ??
+                        "<p>Course description</p>",
+                  ),
                 ],
               ),
             ),
