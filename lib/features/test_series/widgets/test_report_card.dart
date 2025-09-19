961206dc7638c9891fabbf679478e7fd3f8902e4
Midhun Murali <midhunmurali776@gmail.com>
2025-09-19 10:21:41 +0530

    Test Review Screen

---
+++ lib/features/test_series/widgets/test_report_card.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:iconify_flutter_plus/iconify_flutter_plus.dart';
+import 'package:iconify_flutter_plus/icons/material_symbols.dart';
+import 'package:zephyr/constants/app_constants.dart';
+
+class TestReportCard extends StatelessWidget {
+  final String date;
+  final String duration;
+  final String questionno;
+
+  const TestReportCard({super.key, required this.date, required this.duration, required this.questionno});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.primaryBlue),
+      child: Column(
+        children: [
+          SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Row(
+              children: [
+                Iconify(
+                  MaterialSymbols.design_services,
+                  color: AppColors.white,
+                ),
+                SizedBox(
+                  width: 6,
+                ),
+                Text(
+                  "Weekly Test Series 1",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: AppColors.white,
+                      fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 5),
+          Divider(),
+          SizedBox(height: 5),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Duration:",
+                        style: TextStyle(color: AppColors.white)),
+                    Text("Total Questions:",
+                        style: TextStyle(color: AppColors.white))
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text("Date",
+                        style: TextStyle(color: AppColors.white)),
+                    Text(date,
+                        style: TextStyle(color: AppColors.white))
+                  ],
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10)),
+              color: AppColors.primaryGreen,
+            ),
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "Passed",
+                style: TextStyle(
+                    color: AppColors.white, fontWeight: FontWeight.w600),
+              ),
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
